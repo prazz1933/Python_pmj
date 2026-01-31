0fda876f2ecb9161e2c42a85f63f27e79ac9aded
prasanna <pkjammaladinni@edunetfoundation.org>
2026-01-31 15:52:26 +0530

    decorator file changed

--- decorators.py
+++ decorators.py
@@ -1,3 +1,5 @@
 def display(func):
     def wrapper(*args, **kwargs):
         print("hello everyone")
+        print(f"Function '{func.__name__}' is about to be called.")
+        result = func(*args, **kwargs)

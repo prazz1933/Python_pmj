c65fa0ad3971a682ed0a2ebab175fd7b705d9162
prasanna <pkjammaladinni@edunetfoundation.org>
2026-01-31 15:59:00 +0530

    reverted

--- decorators.py
+++ decorators.py
@@ -1,5 +1,4 @@
 def display(func):
     def wrapper(*args, **kwargs):
         print("hello everyone")
-        print(f"Function '{func.__name__}' is about to be called.")
-        result = func(*args, **kwargs)
+        
